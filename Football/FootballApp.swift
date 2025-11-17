ebc399241c36ac78a7d3d09ccc65a90d4fb6dea6
Paul Wagstaff <paul@pwfotos.ca>
2025-11-17 12:44:54 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Paul Wagstaff on 2025-11-17.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
